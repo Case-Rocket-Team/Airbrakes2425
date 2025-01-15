00194caecbadf7962566744ac9dbe66282892ccd
jackfinkcrt <fireblast.fink@gmail.com>
2025-01-14 19:43:59 -0500

    Add files via upload
    maps input altitude and velocity to an angle (eventually)

---
+++ src/AngleMapper.cpp
@@ -0,0 +1,28 @@
+float angleMapper()
+{
+    float inputAlt;
+    float inputVel;
+
+    //make dummy matrix 6000-9200m 0-300m/s
+    float mapMatrix[320][60];
+    
+    for (int i = 0; i < 320; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            mapMatrix[i][j] = (100 * i) - (10 * j);
+        }
+    }
+    
+    //max value conditions
+    if (inputAlt > 9200)
+    {
+        return 90;
+    }
+    if (inputVel > 300)
+    {
+        return 90;
+    }
+
+    return mapMatrix[int((inputAlt - 6000) / 10)][int(inputVel / 5)];
+}
