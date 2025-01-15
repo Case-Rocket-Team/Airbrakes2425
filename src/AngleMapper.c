3c7bfefa37234887a0589b0223e75310baac91db
jackfinkcrt <fireblast.fink@gmail.com>
2025-01-14 19:51:27 -0500

    Add files via upload
    (use this one) maps input altitude and velocity to angle

---
+++ src/AngleMapper.c
@@ -0,0 +1,31 @@
+float angleMapper()
+{
+    float inputAlt;
+    float inputVel;
+
+    //make dummy matrix 6000-9200m 0-300m/s
+    float mapMatrix[320][60];
+    
+    for (int i = 0; i < 320; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            mapMatrix[i][j] = (100 * i) - (10 * j);
+        }
+    }
+    
+    //max value conditions
+    if (inputAlt > 9200)
+    {
+        return 90;
+    }
+    if (inputVel > 300)
+    {
+        return 90;
+    }
+
+    int n = (inputAlt - 6000) / 10;
+    int m = inputVel / 5;
+
+    return mapMatrix[n][m];
+}
