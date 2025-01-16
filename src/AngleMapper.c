6acb2737d2ee8a4bb3d62391217898bfa0908ee4
jackfinkcrt <fireblast.fink@gmail.com>
2025-01-16 13:34:13 -0500

    Update AngleMapper.c

--- src/AngleMapper.c
+++ src/AngleMapper.c
@@ -1,8 +1,10 @@
 float angleMapper(float inputAlt, float inputVel)
 {
-    //make dummy matrix 6000-9200m 0-300m/s
+    //range of inputs expected: 6000-9200m 0-300m/s, lookup table has increments of 10m and 5m/s
+    //make lookup tabke matrix
     float mapMatrix[320][60];
     
+    //dummy matrix
     for (int i = 0; i < 320; i++)
     {
         for (int j = 0; j < 60; j++)
@@ -11,7 +13,7 @@ float angleMapper(float inputAlt, float inputVel)
         }
     }
     
-    //max value conditions
+    //max value conditions, returns 90 deg airbrake angle if above 9200m or faster than 300m/s
     if (inputAlt > 9200)
     {
         return 90;
@@ -21,8 +23,10 @@ float angleMapper(float inputAlt, float inputVel)
         return 90;
     }
 
+    //convert altitude and speed readings to nearest increment in lookup table
     int n = (inputAlt - 6000) / 10;
     int m = inputVel / 5;
 
+    //find the corresponding point in lookup table
     return mapMatrix[n][m];
 }
