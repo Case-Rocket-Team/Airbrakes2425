ba6849a1d93b06d05e2d1703868edeab46730f37
Dhruv <71356377+Dhruv-Raman@users.noreply.github.com>
2025-02-11 18:31:59 -0500

    Update encoder_counts.py

--- src/encoder_counts.py
+++ src/encoder_counts.py
@@ -2,7 +2,7 @@ import math
 
 def convert_motor_angle_to_encoder_counts(initial_angle):
     distance = 2*math.cos(initial_angle) + math.sqrt(3.0625 - (2*math.sin(initial_angle) - 0.35)**2)
-    motor_revolutions = distance / (2*3.14*2)
+    motor_revolutions = distance * 8
     encoder_counts = motor_revolutions * 921.744
 
     return encoder_counts
