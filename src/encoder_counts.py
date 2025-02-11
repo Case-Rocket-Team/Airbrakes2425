e724a0a2c116c07d769208defcca8e481558d861
thebeston <68352501+thebeston@users.noreply.github.com>
2025-02-11 18:25:45 -0500

    Create encoder_counts.py
    Created the function to convert motor revolutions to encoder counts

---
+++ src/encoder_counts.py
@@ -0,0 +1,8 @@
+import math
+
+def convert_motor_angle_to_encoder_counts(initial_angle):
+    distance = 2*math.cos(initial_angle) + math.sqrt(3.0625 - (2*math.sin(initial_angle) - 0.35)**2)
+    motor_revolutions = distance / (2*3.14*2)
+    encoder_counts = motor_revolutions * 921.744
+
+    return encoder_counts
